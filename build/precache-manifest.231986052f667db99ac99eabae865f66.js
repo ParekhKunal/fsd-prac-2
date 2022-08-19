f624d657974d4eee8427203cd9bfd70dfc7d2e5f
parekhkunal <parekhkunal09@gmail.com>
2022-08-19 17:53:07 +0530

    First Commit

---
+++ build/precache-manifest.231986052f667db99ac99eabae865f66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18bc709a3154dc1a9a65ce95c5c60b9d",
+    "url": "/fsd-prac-2/index.html"
+  },
+  {
+    "revision": "a1e4dc090d8afff203c8",
+    "url": "/fsd-prac-2/static/css/main.cb669b7f.chunk.css"
+  },
+  {
+    "revision": "ae60aecbf2c17fc7d44d",
+    "url": "/fsd-prac-2/static/js/2.436dbd77.chunk.js"
+  },
+  {
+    "revision": "bec4545363a05ee9f8956b666013b4ab",
+    "url": "/fsd-prac-2/static/js/2.436dbd77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1e4dc090d8afff203c8",
+    "url": "/fsd-prac-2/static/js/main.bd47b93a.chunk.js"
+  },
+  {
+    "revision": "a2175c90d04b3a889240",
+    "url": "/fsd-prac-2/static/js/runtime-main.03af3081.js"
+  }
+]);
